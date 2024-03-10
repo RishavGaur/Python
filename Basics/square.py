8509624c30c983a6296a6c4456eaff683ada2b72
RishavGaur <rishav021gaur@gmail.com>
2024-03-10 11:48:36 +0530

    Square of a number

---
+++ Basics/square.py
@@ -0,0 +1,5 @@
+''' Write a python script to calculate square of a given number
+    (number is taken from user) '''
+
+num= eval(input("Enter a Number to find Square: "))
+print("Square is",num**2)
