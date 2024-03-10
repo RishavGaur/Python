712f6efbf1727e5a3bdb956ac3c89ede5107760b
RishavGaur <rishav021gaur@gmail.com>
2024-03-10 12:10:23 +0530

    Checking divisibility of 5

---
+++ Decision-Control/divisibility.py
@@ -0,0 +1,4 @@
+''' Write a python script to check whether a given number is 
+    divisible by 5 or not. '''
+
+print("It is Divisible by 5" if eval(input("Enter a Number: "))%5==0 else "Number is not divisible by 5")
