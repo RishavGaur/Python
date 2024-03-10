2b85047e1b3eccafa86b9172e6291c92a4e4843f
RishavGaur <rishav021gaur@gmail.com>
2024-03-10 11:55:15 +0530

    Area of Triangle

---
+++ Basics/triangle.py
@@ -0,0 +1,8 @@
+''' Write a python script to calculate area of a triangle.
+ Lengths of the sides are given by user. '''
+
+print("Feed data to find Area of Triangle...")
+base= eval(input("Enter Base: "))
+height= eval(input("Enter Height: "))
+
+print("Area of Triangle=",(1/2*base*height))
