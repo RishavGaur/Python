58b72349f2471b468589656d9b42f74925b37d51
RishavGaur <rishav021gaur@gmail.com>
2024-03-10 12:41:17 +0530

    Converting Month into Days

---
+++ Decision-Control/monthToDays.py
@@ -0,0 +1,5 @@
+''' Write a python script to take month value in numeric 
+    format and display number of days in it. '''
+
+months= int(input("Enter Number of Months: "))
+print("Number of Days in %d month have %d days"%(months,months*30))
