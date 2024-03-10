327984b9b48d5e9a57ef1442637ee4cf8bbfab42
RishavGaur <rishav021gaur@gmail.com>
2024-03-10 12:16:36 +0530

    Number checking

---
+++ Decision-Control/numberCheck.py
@@ -0,0 +1,11 @@
+''' Write a python script to check whether a given number
+    is positive, negative or zero '''
+
+num= eval(input("Enter a Number: "))
+
+if num>0:
+    print("Positive Number")
+elif num<0:
+    print("Negative Number")
+else:
+    print("Number is zero")
